8c10308491ecdaddfbf30eb0fb8aca821e2902d3
krunom01 <krunom92@gmail.com>
2019-01-25 01:01:46 +0100

    array with DBdata

---
+++ DBdata.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kruno
+ * Date: 25/01/19
+ * Time: 0.58
+ */
+$DBdata = array(
+    'host'=>'localhost',
+    'dbname'=>'employee',
+    'user'=>'kruno',
+    'password'=>'xxxxxx'
+);
