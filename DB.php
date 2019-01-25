ad306c5c24f266f8eac1c5d52ac888c456e4f11b
krunom01 <krunom92@gmail.com>
2019-01-25 12:14:21 +0100

    DB singleton, test ok

--- DB.php
+++ DB.php
@@ -6,30 +6,32 @@
  * Time: 1.21
  */
 
-require 'getDB.php';
 
 class DB
 {
+    private static $instance = null;
     private $PDO;
-
-
+    private $host = 'localhost';
+    private $dbname = 'employee';
+    private $username = 'kruno';
+    private $password = 'kruno031';
 
     public function __construct()
     {
-        try {
-            $this->PDO = new PDO(
-                'mysql:host=' . getDB::getDBvalues('host') . ';
-                dbname=' . getDB::getDBvalues('dbname'),
-                getDB::getDBvalues('user'),
-                getDB::getDBvalues('password'));
-        } catch (PDOException $exception) {
-            die($exception->getMessage());
-        }
-        echo "connected";
+            $this->PDO = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname, $this->username, $this->password);
     }
+    public static function instance()
+    {
+        if(!self::$instance){ //check if class DB is already created,if not,create new one.
+            self::$instance = new DB();
+        }
+        return self::$instance;
+    }
+    public function connection(){
+        return $this->PDO;
+    }
+
 }
 
-
-$DB = new DB;
 
 
